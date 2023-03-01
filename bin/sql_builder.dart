b8a8885c18cebfdf9b9c00e45e281bcc755c3b54
Emmanuel Villalobos <emmanuel.villalobos@gmail.com>
2023-03-01 14:46:35 -0500

    Initial commit

---
+++ bin/sql_builder.dart
@@ -0,0 +1,5 @@
+import 'package:sql_builder/sql_builder.dart' as sql_builder;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sql_builder.calculate()}!');
+}
---
+++ test/sql_builder_test.dart
@@ -0,0 +1,8 @@
+import 'package:sql_builder/sql_builder.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
